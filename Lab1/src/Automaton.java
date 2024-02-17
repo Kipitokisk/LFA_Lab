18ef3959c35375a1bc1cefa00a95b49f713f74e4
Victor Revenco <victor.revenco@isa.utm.md>
2024-02-17 15:48:10 +0200

    Add attributes and constructor to Automaton class

--- Lab1/src/Automaton.java
+++ Lab1/src/Automaton.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class Automaton {
+    private Set<String> states;
+    private Set<String> alphabet;
+    private Map<String, Map<String, String>> transitions;
+    private String initialState;
+    private Set<String> finalStates;
+
+    public Automaton(Set<String> states, Set<String> alphabet, Map<String, Map<String, String>> transitions,
+                           String initialState, Set<String> finalStates) {
+        this.states = states;
+        this.alphabet = alphabet;
+        this.transitions = transitions;
+        this.initialState = initialState;
+        this.finalStates = finalStates;
+    }
+
 }
