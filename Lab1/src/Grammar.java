f2ec7734f49cf92fb2ec78111ed681d8688cbe2e
Victor Revenco <victor.revenco@isa.utm.md>
2024-02-17 15:20:18 +0200

    Create raw version of Grammar class

--- Lab1/src/Grammar.java
+++ Lab1/src/Grammar.java
@@ -1,2 +1,25 @@
+import java.util.*;
+
 public class Grammar {
+    private Set<String> nonTerminals;
+    private Set<String> terminals;
+    private Map<String, List<String>> productions;
+
+    public Grammar(Set<String> nonTerminals, Set<String> terminals, Map<String, List<String>> productions) {
+        this.nonTerminals = nonTerminals;
+        this.terminals = terminals;
+        this.productions = productions;
+    }
+
+    public Set<String> getNonTerminals() {
+        return nonTerminals;
+    }
+
+    public Set<String> getTerminals() {
+        return terminals;
+    }
+
+    public Map<String, List<String>> getProductions() {
+        return productions;
+    }
 }
