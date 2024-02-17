2889c0caaa7ccc0124f2a958c705f94721ee0327
Victor Revenco <victor.revenco@isa.utm.md>
2024-02-17 15:25:17 +0200

    Create instance of grammar in Main

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,2 +1,14 @@
+import java.util.*;
+
 public class Main {
+    public static void main (String[] args) {
+        Set<String> nonTerminals = new HashSet<>(Arrays.asList("S", "A", "B", "C"));
+        Set<String> terminals = new HashSet<>(Arrays.asList("a", "b", "c", "d"));
+        Map<String, List<String>> productions = new HashMap<>();
+        productions.put("S", Collections.singletonList("dA"));
+        productions.put("A", Collections.singletonList("aB"));
+        productions.put("B", Arrays.asList("bC", "d"));
+        productions.put("C", Arrays.asList("cB", "aA"));
+        Grammar grammar = new Grammar(nonTerminals, terminals, productions);
+    }
 }
